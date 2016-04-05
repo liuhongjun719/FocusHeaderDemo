2b6bb8fb8c393078bd9abfce0e6c2d3ac3e10f18
YouCanDoIt <a123456@12306demac-mini.local>
2016-04-05 09:24:08 +0800

    Initial Commit

---
+++ FocusHeaderDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FocusHeaderDemo
+//
+//  Created by 123456 on 16/4/5.
+//  Copyright © 2016年 123456. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
